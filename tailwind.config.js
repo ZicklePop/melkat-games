6263dc619728778a834656b531e0526781366ed9
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-12-18 23:37:41 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   mode: 'jit',
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: 'media',
-  theme: {
-    extend: {}
-  },
-  variants: {
-    extend: {}
-  },
-  plugins: []
+  darkMode: 'media'
 }
